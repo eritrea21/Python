8bbd1e3df2eacad138084db66ff18ce68d7fd7ea
Mohammed Idris <mohjj46@gmail.com>
2021-11-21 04:48:51 -0600

    Variable_demo2

---
+++ Variables/variable_demo2.py
@@ -0,0 +1,10 @@
+# Create two variables: top_speed and distance.
+top_speed = 160
+distance = 300
+
+
+# display the values of referenced by the variables
+print ("The top speed is : ")
+print(top_speed)
+print('The distance is : ')
+print(distance)
