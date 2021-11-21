ba3621eb62535cd512f10af831cedd72e32ed790
Mohammed Idris <mohjj46@gmail.com>
2021-11-21 04:17:29 -0600

    Var-1

---
+++ Variables/variable_demo.py
@@ -0,0 +1,5 @@
+# A variable is a name that represents a value stored in the computer's memory.
+# This program demonstrates a variable
+room = 503
+print('I am staying in room number')
+print(room)
