3d99174973588aba6b39dc25f536421851b5af8f
Mohammed Idris <mohjj46@gmail.com>
2021-11-21 03:22:50 -0600

    End-line Comment

---
+++ CHAPTER-01/comment2.py
@@ -0,0 +1,4 @@
+# end-line comment
+print('Anna karina')            # displays the name
+print('123 Full Circle Drive')  # displays the address
+print('Memphis, TN, 38122')     # displays the city, state and ZIP
