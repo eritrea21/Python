94051032803416b2eacdb97cbed8d8453634dc41
Mohammed Idris <mohjj46@gmail.com>
2021-11-21 03:02:09 -0600

    Exercise-1

---
+++ CHAPTER-01/ex-1.py
@@ -0,0 +1,6 @@
+# write statement that displays your name 
+print("My name is Mohammed!")
+# write statement that displays the following  text
+print("Python's the best!")
+# write statement that displays the following text
+print('The cat said "meow!" ')
