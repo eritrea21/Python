5dd08c3d98b11382e53b926c7b9f884048463bcd
Mohammed Idris <mohjj46@gmail.com>
2021-11-21 02:49:03 -0600

    Display Quotes

---
+++ CHAPTER-01/display_quote.py
@@ -0,0 +1,9 @@
+# Display Qutoes
+print('Your assignment is to read "Hamlet" by tomorrow.')
+# python allows to enclose string literals in triple qoutes(either """ or ''')
+print("""I'm reading "The Lord Of The Ring" tonight """)
+#Triple quotes can be used to surround multiline strings
+print(""" one 
+      Two
+      Three
+      """)
