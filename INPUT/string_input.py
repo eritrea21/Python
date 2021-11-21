3bc528a1098771476c9ceb0b73dfeb70b95fa437
Mohammed Idris <mohjj46@gmail.com>
2021-11-21 06:30:13 -0600

    Input Example

---
+++ INPUT/string_input.py
@@ -0,0 +1,11 @@
+# Get the user's first name
+# input is 
+# variable = input(prompt)
+first_name = input('Enter your first name : ')
+
+# get the user's last name
+last_name = input('Enter your last name')
+
+# print the greeting to the user
+
+print('Hello', first_name, last_name)
